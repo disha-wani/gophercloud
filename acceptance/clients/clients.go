f8a78b47d88171b3e24cb06d02f3d7d01c718560
d-apurva <apurva.dhoble@click2cloud.net>
2018-01-10 17:20:54 +0530

    updated clients.go for acceptance test

--- acceptance/clients/clients.go
+++ acceptance/clients/clients.go
@@ -136,27 +136,7 @@ func NewBlockStorageV2Client() (*gophercloud.ServiceClient, error) {
 	})
 }
 
-// NewVpcV1Client returns a *ServiceClient for making calls to the
-// OTC Vpc v1 API. An error will be returned if authentication
-// or client creation was not possible.
-func NewVpcV1Client() (*gophercloud.ServiceClient, error) {
-	ao, err := openstack.AuthOptionsFromEnv()
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := openstack.AuthenticatedClient(ao)
-	if err != nil {
-		return nil, err
-	}
-
-	return openstack.NewVpcV1(client, gophercloud.EndpointOpts{
-		Region: os.Getenv("OS_REGION_NAME"),
-	})
-}
-
-
-// NewVpcV1Client returns a *ServiceClient for making calls to the
+// NewSubnetV1Client returns a *ServiceClient for making calls to the
 // OTC Subnet v1 API. An error will be returned if authentication
 // or client creation was not possible.
 func NewSubnetV1Client() (*gophercloud.ServiceClient, error) {
